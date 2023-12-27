bec70cbe3a0c070ebd4e987b6a4f37f2cc2e3d38
mb-tousif <meta.block.tousif@gmail.com>
2023-12-27 20:43:38 +0600

    user module added

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,21 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-export type UserDocument = HydratedDocument<User>
+export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop()
-  name: string;
+    @Prop( { required: true } )
+    name: string;
 
-  @Prop()
-  email: string;
+    @Prop( { required: true } )
+    email: string;
 
-  @Prop()
-  password: string;
+    @Prop( { required: true } )
+    password: string;
 
-  @Prop()
-  role: string;
+    @Prop( { required: true } )
+    role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
