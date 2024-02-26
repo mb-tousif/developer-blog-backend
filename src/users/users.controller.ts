f52fe125c48ee6548833f0d3df2c36629637ed08
mb-tousif <meta.block.tousif@gmail.com>
2024-02-26 20:07:36 +0600

    ---

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,16 +2,16 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './users.dto';
 
-@Controller('users')
+@Controller(`users`)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @Post(`create-user`)
-  async createUser(@Body() userDto: UserDto) {
-    return this.usersService.createUser(userDto);
-  }
-
   @Get(`all-users`)
   async getAllUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @Post(`create-user`)
+  async createUser(@Body() userDto: UserDto) {
+    return this.usersService.createUser(userDto);
   }
 }
